deebd3cd63f40aa1eb6defeef8c028a297e26c61
Rojhat <86376642+RojhatSef@users.noreply.github.com>
2022-11-24 15:06:28 +0100

    Step before applying ApplicationUser

---
+++ MangaModelService/ViewModels/GroupScanlationModelView.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MangaModelService.ViewModels
+{
+    public class GroupScanlationModelView
+    {
+
+        public int GroupScanlatingID { get; set; }
+        [Required]
+        public string GroupName { get; set; }
+        public string PhotoPath { get; set; }
+        public string website { get; set; }
+        public ICollection<ChapterModel>? chapterModels { get; set; }
+        public int MasterID { get; set; }
+        public ICollection<MasterModel>? MasterModels { get; set; }
+        public string userID { get; set; }
+        public ICollection<UserModel>? userModels { get; set; }
+    }
+}
---
+++ NovelXManga/Pages/GroupScan/AddGroupScanlation.cshtml.cs
@@ -0,0 +1,50 @@
+using MangaAccessService;
+using MangaModelService;
+using MangaModelService.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NovelXManga.Pages.GroupScan
+{
+    public class AddGroupScanlationModel : PageModel
+    {
+        private readonly MangaNNovelAuthDBContext mangaNNovelAuthDBContext;
+        private readonly IWebHostEnvironment webHostEnvironment;
+        private readonly UserManager<IdentityUser> userManager;
+        public AddGroupScanlationModel(IWebHostEnvironment webHostEnvironment, MangaNNovelAuthDBContext mangaNNovelAuthDBContext, UserManager<IdentityUser> userManager)
+        {
+            this.webHostEnvironment = webHostEnvironment;
+            this.mangaNNovelAuthDBContext = mangaNNovelAuthDBContext;
+            this.userManager = userManager;
+        }
+        [BindProperty]
+        public GroupScanlationModelView groupModelView { get; set; }
+        public async Task<IActionResult> OnGet(string id)
+        {
+            if (id != null)
+            {
+                var groupUser = await userManager.GetUserAsync(User);
+                var userId = groupUser?.Id;
+
+            }
+            return Page();
+
+        }
+        public IActionResult OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var newGroup = mangaNNovelAuthDBContext.groupScanlatingModels.FirstOrDefault(gcm => gcm.GroupName == groupModelView.GroupName);
+                if (newGroup != null)
+                {
+                    GroupScanlatingModel groupScanlatingModel = new GroupScanlatingModel()
+                    {
+                        GroupName = groupModelView.GroupName,
+                        chapterModels = null,
+                    }
+                }
+            }
+        }
+    }
+}
