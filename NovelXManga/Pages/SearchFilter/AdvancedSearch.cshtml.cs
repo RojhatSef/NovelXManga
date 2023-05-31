09e9037426538b34c6c2f9d31f52b85d4c153299
Rojhat <86376642+RojhatSef@users.noreply.github.com>
2023-05-31 16:38:35 +0200

    Starting on Advanced Search

--- NovelXManga/Pages/SearchFilter/AdvancedSearch.cshtml.cs
+++ NovelXManga/Pages/SearchFilter/AdvancedSearch.cshtml.cs
@@ -1,11 +1,15 @@
 using MangaAccessService;
+using MangaModelService;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace NovelXManga.Pages.SearchFilter
 {
     public class AdvancedSearchModel : PageModel
     {
-        private readonly MangaNNovelAuthDBContext Context;
+        private readonly MangaNNovelAuthDBContext context;
         private readonly IMangaRepository mangaRepository;
         private readonly IAuthorRepsitory authorRepsitory;
         private readonly IArtistRepsitory artistRepsitory;
@@ -16,9 +20,37 @@ namespace NovelXManga.Pages.SearchFilter
         private readonly ILanguageRepsitory languageRepsitory;
         private readonly IStudioRepsitory studioRepsitory;
 
+        [BindProperty]
+        public IFormFile? Photo { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM}", ApplyFormatInEditMode = true)]
+        [BindProperty]
+        public DateTime ReleaseYear { get; set; } = DateTime.Now;
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM}", ApplyFormatInEditMode = true)]
+        [BindProperty]
+        public DateTime? EndingYear { get; set; } = null;
+
+        [BindProperty]
+        public MangaModel _MangaModel { get; set; }
+
+        [BindProperty]
+        [FromForm(Name = "selectedTags")]
+        public List<int> SelectedTags { get; set; }
+
+        [BindProperty]
+        [FromForm(Name = "selectedGenres")]
+        public List<int> SelectedGenres { get; set; }
+
+        public List<TagModel> Tags { get; set; }
+        public List<GenresModel> Genres { get; set; }
+        public IEnumerable<MangaModel> MangaModels { get; set; }
+
         public AdvancedSearchModel(MangaNNovelAuthDBContext context, IMangaRepository mangaRepository, IAuthorRepsitory authorRepsitory, IArtistRepsitory artistRepsitory, ITagRepsitory tagRepsitory, IGenreRepsitory genreRepsitory, IChapterModelRepsitory chapterRepsitory, IAssociatedNamesRepsitory associatedNamesRepsitory, ILanguageRepsitory languageRepsitory)
         {
-            Context = context;
+            this.context = context;
             this.mangaRepository = mangaRepository;
             this.authorRepsitory = authorRepsitory;
             this.artistRepsitory = artistRepsitory;
@@ -29,8 +61,18 @@ namespace NovelXManga.Pages.SearchFilter
             this.languageRepsitory = languageRepsitory;
         }
 
-        public void OnGet()
+        public async Task<IActionResult> OnPostAsync()
         {
+            return
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Tags = await context.TagModels.ToListAsync();
+            Genres = await context.GenresModels.ToListAsync();
+            MangaModels = await mangaRepository.GetAllModelAsync();
+
+            return Page();
         }
     }
 }
