0124aa6f2bce4d1d7aea4490cd4920d5d260772e
Rojhat <86376642+RojhatSef@users.noreply.github.com>
2024-01-21 23:10:12 +0100

    Updating for laptop. Created usersettingDTO

---
+++ MangaAccessService/DTO/UserSettingsDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MangaAccessService.DTO
+{
+    public class UserSettingsDTO
+    {
+        public string UserModelId { get; set; }
+        public bool ShowAdultContent { get; set; }
+        public bool ShowMatureContent { get; set; }
+    }
+}
