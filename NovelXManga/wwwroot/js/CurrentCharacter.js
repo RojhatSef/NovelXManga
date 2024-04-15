03e4a0d92ca89be77999311394f10e037261e29d
RojhatSefdin <Rojhat10@hotmail.com>
2024-04-15 12:08:16 +0200

    Forgott some data on currentmanga, also famous quote had some stupid overflow. fixed.

---
+++ NovelXManga/wwwroot/js/CurrentCharacter.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const container = document.querySelector('.CUS-MangaContainerInputQuote');
+    if (container) {
+        const lineHeight = 15; 
+        const observer = new ResizeObserver(entries => {
+            for (let entry of entries) {
+                const rows = Math.ceil(entry.target.scrollHeight / lineHeight);
+                container.style.height = `${rows * lineHeight}px`; 
+            }
+        });
+        observer.observe(container);
+    }
+});
